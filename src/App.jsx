93c9068a438c6f35fa2dbe602dc0b26d64583f37
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-02-13 17:48:31 +0100

    Add: GlobalProvider in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DefaultLayouts from "./layouts/DefaultLayouts"
 import TravelsPage from "./pages/TravelsPage"
 import ParticipantsPage from "./pages/ParticipantsPage"
+import { GlobalProvider } from "./context/GlobalContext"
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={DefaultLayouts}>
-          <Route path="/" element={TravelsPage} />
-          <Route path="/travel/:id" element={ParticipantsPage} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={DefaultLayouts}>
+            <Route path="/" element={TravelsPage} />
+            <Route path="/travel/:id" element={ParticipantsPage} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
   )
 }
 
