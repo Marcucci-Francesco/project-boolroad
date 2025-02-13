9c760c8b79a4b95d3674be0ee8b51d7ef29aa486
joelepore <giuseppejoelepore@gmail.com>
2025-02-13 18:20:43 +0100

    feat: create basic ParticipantsPage layout

--- src/Components/PartecipantsAccordion.jsx
+++ src/Components/PartecipantsAccordion.jsx
@@ -4,28 +4,28 @@ import Accordion from 'react-bootstrap/Accordion';
 const AccordionParticipants = ({ participants }) => {
   return (
     <Accordion defaultActiveKey="0">
-    {participants.map((participant, index) => 
-      <Accordion.Item eventKey={index}>
-        <Accordion.Header>{participant.nome} {participant.cognome}</Accordion.Header>
-        <Accordion.Body>     
-        
-          <p>CF: {participant.codiceFiscale}</p>
-       
-          <p>Telefono: {participant.numeroDiTelefono}</p>
+      {participants.map((participant, index) =>
+        <Accordion.Item eventKey={index} key={index}>
+          <Accordion.Header>{participant.nome} {participant.cognome}</Accordion.Header>
+          <Accordion.Body>
 
-          <p>E-Mail:{}</p>
-          
-          <ul><h3>Contatto d'Emergenza</h3>
-            <li>{participant.contattoDiEmergenza.nome}</li>
-            <li>{participant.contattoDiEmergenza.cognome}</li>
-            <li>{participant.contattoDiEmergenza.numeroDiTelefono}</li>
-            <li>{participant.contattoDiEmergenza.mail}</li>
+            <p>CF: {participant.codiceFiscale}</p>
 
-          </ul>
+            <p>Telefono: {participant.numeroDiTelefono}</p>
 
-          
-        </Accordion.Body>        
-      </Accordion.Item>
+            <p>E-Mail:{ }</p>
+
+            <ul><h3>Contatto d'Emergenza</h3>
+              <li>{participant.contattoDiEmergenza.nome}</li>
+              <li>{participant.contattoDiEmergenza.cognome}</li>
+              <li>{participant.contattoDiEmergenza.numeroDiTelefono}</li>
+              <li>{participant.contattoDiEmergenza.mail}</li>
+
+            </ul>
+
+
+          </Accordion.Body>
+        </Accordion.Item>
       )}
     </Accordion>
 
--- src/pages/ParticipantsPage.jsx
+++ src/pages/ParticipantsPage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AccordionParticipants from '../Components/PartecipantsAccordion'
+import { useGlobalContext } from '../context/GlobalContext';
+import { useParams } from 'react-router-dom';
 
 const ParticipantsPage = () => {
+  const { id } = useParams();
+  const { partecipants, fetchPartecipants, travels, fetchTravels } = useGlobalContext();
+
+  useEffect(() => {
+    console.log(id);
+    fetchPartecipants(parseInt(id));
+  }, []);
+
   return (
-    <div>ParticipantsPage</div>
+    <>
+      <div className="container">
+        <form className="d-flex mb-3">
+          <input className="form-control me-2" type="search" placeholder="Filtra i partecipanti" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
+
+        <h2>Lista dei partecipanti</h2>
+
+        {partecipants && <AccordionParticipants participants={partecipants} />}
+      </div>
+    </>
   )
 }
 
