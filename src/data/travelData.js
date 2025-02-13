44ba96390bc615f6703e35f3fcf3436e8d7fe3aa
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-02-13 16:54:09 +0100

    add: dati in travelData

---
+++ src/data/travelData.js
@@ -0,0 +1,78 @@
+const travels = [
+    {
+        ID: 1,
+        destination: "Roma",
+        startDate: "2025-03-10T00:00:00.000Z",
+        endDate: "2025-03-15T00:00:00.000Z",
+        tourLeader: "Marco Rossi",
+        participants: [
+            {
+                nome: "Giulia",
+                cognome: "Bianchi",
+                codiceFiscale: "BNCGLL98A41H501K",
+                numeroDiTelefono: "3456789012",
+                contattoDiEmergenza: {
+                    nome: "Luca",
+                    cognome: "Bianchi",
+                    numeroDiTelefono: "3298765432",
+                    mail: "luca.bianchi@email.com"
+                }
+            },
+            {
+                nome: "Andrea",
+                cognome: "Verdi",
+                codiceFiscale: "VRDNDR85C11F205X",
+                numeroDiTelefono: "3467890123",
+                contattoDiEmergenza: {
+                    nome: "Francesca",
+                    cognome: "Verdi",
+                    numeroDiTelefono: "3287654321",
+                    mail: "francesca.verdi@email.com"
+                }
+            }
+        ]
+    },
+    {
+        ID: 2,
+        destination: "Parigi",
+        startDate: "2025-04-05T00:00:00.000Z",
+        endDate: "2025-04-12T00:00:00.000Z",
+        tourLeader: "Anna Ferrari",
+        participants: [
+            {
+                nome: "Lorenzo",
+                cognome: "Ricci",
+                codiceFiscale: "RCCLRN90D15H501W",
+                numeroDiTelefono: "3478901234",
+                contattoDiEmergenza: {
+                    nome: "Marta",
+                    cognome: "Ricci",
+                    numeroDiTelefono: "3276543210",
+                    mail: "marta.ricci@email.com"
+                }
+            }
+        ]
+    },
+
+    {
+        ID: 3,
+        destination: "Berlino",
+        startDate: "2025-04-05T00:00:00.000Z",
+        endDate: "2025-04-12T00:00:00.000Z",
+        tourLeader: "Anna Verdi",
+        participants: [
+            {
+                nome: "Marco",
+                cognome: "Ricci",
+                codiceFiscale: "RCCLRN90D15H501W",
+                numeroDiTelefono: "3478901234",
+                contattoDiEmergenza: {
+                    nome: "Marta",
+                    cognome: "Ricci",
+                    numeroDiTelefono: "3276543210",
+                    mail: "marta.ricci@email.com"
+                }
+            }
+        ]
+    }
+];
