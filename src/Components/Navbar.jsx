e3c827ee78c766d8dcb81bab802a957b42a54538
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-02-14 11:57:13 +0100

    [Add] logo, css del logo e navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,10 +6,13 @@ const Navbar = () => {
 
   return (
     <div className="container-fluid bg-dark d-flex justify-content-center align-items-center py-4 mb-5">
-      <div className="logo-container">
-        <img className="logo img-fluid" src="" alt="logo" onClick={() => navigate("/")} />
+      <div className="logo-container d-flex align-items-center">
+        <img className="logo img-fluid" src="/img/logo.png" alt="logo" onClick={() => navigate("/")} />
+        <h4 className="text-primary text-center ms-3"><strong className="boolroad">BOOLROAD</strong></h4>
       </div>
     </div>
+
+
 
   )
 }
