8a99e55520ff2a5b79cf6811d1bb74b31facad22
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-02-14 11:35:33 +0100

    [Add] navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
 const Navbar = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>Navbar</div>
+    <div className="container-fluid bg-dark d-flex justify-content-center align-items-center py-4 mb-5">
+      <div className="logo-container">
+        <img className="logo img-fluid" src="" alt="logo" onClick={() => navigate("/")} />
+      </div>
+    </div>
+
   )
 }
 
