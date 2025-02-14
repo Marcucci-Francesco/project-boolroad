ae797d4f7eb74f623d594fb30b5375dd45f0c043
joelepore <giuseppejoelepore@gmail.com>
2025-02-14 12:30:33 +0100

    fix: adjust vertical alignment in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="container-fluid bg-dark d-flex justify-content-center align-items-center py-4">
       <div className="logo-container d-flex align-items-center" onClick={() => navigate("/")}>
         <img className="logo img-fluid" src="/img/logo.png" alt="logo" />
-        <h4 className="text-primary text-center ms-3 boolroad-type-logo"><strong className="boolroad">BOOLROAD</strong></h4>
+        <strong className="boolroad-type-logo text-primary">BOOLROAD</strong>
       </div>
     </div>
 
