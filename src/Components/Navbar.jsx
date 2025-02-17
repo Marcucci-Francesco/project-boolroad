64942700f4b9d65bedc20572c8f044bf4a415ded
joelepore <giuseppejoelepore@gmail.com>
2025-02-17 10:17:28 +0100

    feat: add third-logo to navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
   return (
     <div className="container-fluid bg-dark d-flex justify-content-center align-items-center py-4">
       <div className="logo-container d-flex align-items-center" onClick={() => navigate("/")}>
-        <img className="logo img-fluid" src="/img/logo.png" alt="logo" />
-        <strong className="boolroad-type-logo text-primary">BOOLROAD</strong>
+        <img className="logo img-fluid" src="/img/Third-logo-transparent.png" alt="logo" />
+        {/* <strong className="boolroad-type-logo text-primary">BOOLROAD</strong> */}
       </div>
     </div>
 
